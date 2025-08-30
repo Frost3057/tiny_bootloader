e060037e626d0d5fdf908f83e34aba1345efd741
Aman Bajpai <amanbajpai5660@gmail.com>
2025-08-31 04:43:29 +0530

    feat: Implement screen clearing functionality in main.asm
    This commit adds the necessary code to the `main.asm` file to clear the screen. The `clearscreen` function now utilizes BIOS interrupt `0x10` with appropriate parameters to clear the entire display.

--- main.asm
+++ main.asm
@@ -12,5 +12,13 @@ clearscreen:
     mov bp,sp
     pusha
     mov ah,0x07 ;this tells the bios to scroll the window down
-    mov 
+    mov al,0x00;clear the entire window
+    mov cx,0x00; tells that the top left of the screen starts from (0,0)
+    mov dh,0x18 ;18h = 24 rows of chars
+    mov dl,0x4f ;4fh = 79 cols of chars
+
+    popa 
+    mov sp,bp
+    ret
+    
 
