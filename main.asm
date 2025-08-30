ee1223e909c088a6b0accbef813bc06f1bf199b9
Aman Bajpai <amanbajpai5660@gmail.com>
2025-08-31 05:09:56 +0530

    feat: Initialize stack and add basic screen output
    This commit initializes the stack segment and pointer for the bootloader. It also adds a `clearscreen` function and a `print` function to display a message on the screen, along with a `movecursor` function.

--- main.asm
+++ main.asm
@@ -1,12 +1,26 @@
-start:
-    mov ax,0x7C0
-    mov ds,as
+bits 16
+;the bootloader loads its data from the rom to ram at position 0x7c0
+mov ax,0x7C0
+mov ds,as
+;the stack segment starts just after the boot segment which is of 512 bytes.
+mov ax,0x7E0
+mov ss,ax
+;now here the stack pointer generally downgrades on each addition and we want a 8kb stack segment therefore we will use the address
+;0x2000 for the stack pointer
+mov sp,0x2000
 
-    mov ax,0x7E0
-    mov ss,ax
+call clearscreen
+push 0x0000
 
-    mov sp,0x2000
+call movecursor
+add sp,2
 
+push msg
+call print
+add sp,2
+
+cli
+hlt
 clearscreen:
 ;Overhead :
     push bp ;we are saving the caller's base pointer
@@ -38,7 +52,29 @@ movecursor:
     mov sp,bp
     pop bp
     ret
+;the 0 at the end works as a null character which can be used to terminate the string.
+msg: db "Oh boy this is tough!",0
+print:
+    push bp
+    mov bp,sp
+    pusha
+    mov si,[bp+4] ;get pointer to the data
+    mov bh,0x00 ;page number,not required here
+    mov bl,0x00 ;foreground color
+    mov ah,0x0eh ;print char to ttv
+.char:
+    mov al,[si] ;get the character from the index
+    add si,1
+    or al,0
+    je .return
+    int 0x10 ;print the character if we are not done
+    jmp .char
+.return:
+    popa
+    mov sp,bp
+    pop bp
+    ret
 
-
-
+times 510-(\$-$$) db 0
+dw 0xAA55
 
